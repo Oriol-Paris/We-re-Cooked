77d3ba4d42ca1ba418706ee628d854f2220639d3
B-Dani <daniel.belmonte@enti.cat>
2025-07-07 14:34:09 +0200

    Main Menu Done

--- Assets/Scripts/Menus/SwapScenes.cs
+++ Assets/Scripts/Menus/SwapScenes.cs
@@ -3,19 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class SwapScenes : MonoBehaviour
 {
-    public string sceneTutoName;
     public string sceneName;
 
     public void SwapScene()
     {
-        if (!PlayerPrefs.HasKey("TutorialComplete")) {
-            SceneManager.LoadScene(sceneTutoName, LoadSceneMode.Single);
-        } else {
-            SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
-        }
-        
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
     }
 
     // Método para salir del juego
