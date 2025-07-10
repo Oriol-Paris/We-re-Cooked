2313c8717d8c4b979bff7d236886d35b86f01939
DANIEL DINU CRIADO <daniel.dinu@enti.cat>
2025-07-10 14:26:39 +0200

    gitflow-feature-stash: ingredient-outlines-type-transition

--- Assets/Scripts/Ingredients_Receipes/Ingredient.cs
+++ Assets/Scripts/Ingredients_Receipes/Ingredient.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 [Serializable]
 public class IngredientMeshes {
     public List<MeshRenderer> meshR;
+    public List<Outline> outlines;
 }
 
 public class Ingredient : MonoBehaviour {
@@ -17,6 +18,12 @@ public class Ingredient : MonoBehaviour {
 
     public List<IngredientMeshes> meshRendL;
 
+    public float colorTransitionSpeed = 15;
+    [SerializeField]
+    private Color currTypeCol;
+    [SerializeField]
+    private int currTypeColIndex = 0;
+
 
     void Start() {
         maxColors = meshRendL.Count;
@@ -24,7 +31,7 @@ public class Ingredient : MonoBehaviour {
         ingredientTypes = new List<IngredientType>();
         ingredientColors = new List<IngredientColor>();
 
-        for (int i = 0; i < ingredientTypes.Count; i++) {
+        for (int i = 0; i < maxTypes; i++) {
             ingredientTypes.Add(IngredientManager.instance.GetRandomType());
         }
 
@@ -33,6 +40,26 @@ public class Ingredient : MonoBehaviour {
             for (int j = 0; j < meshRendL[i].meshR.Count; j++) {
                 meshRendL[i].meshR[j].material.color =
                     IngredientManager.instance.GetColorByIndex((int)ingredientColors[i]);
+            }
+            for (int j = 0; j < meshRendL[i].outlines.Count; j++) {
+                meshRendL[i].outlines[j].OutlineColor = IngredientManager.instance.GetTypeColorByIndex((int)ingredientTypes[0]);
+            }
+        }
+        currTypeCol = IngredientManager.instance.GetTypeColorByIndex((int)ingredientTypes[0]);
+    }
+
+    void FixedUpdate() {
+        if (ingredientTypes.Count <= 1) return;
+
+        currTypeCol = Color.Lerp(currTypeCol, IngredientManager.instance.GetTypeColorByIndex((int)ingredientTypes[currTypeColIndex]), colorTransitionSpeed * Time.deltaTime);
+
+        if (currTypeCol == IngredientManager.instance.GetTypeColorByIndex((int)ingredientTypes[currTypeColIndex])) {
+            currTypeColIndex = (currTypeColIndex + 1) % ingredientTypes.Count;
+        }
+
+        for (int i = 0; i < meshRendL.Count; i++) {
+            for (int j = 0; j < meshRendL[i].outlines.Count; j++) {
+                meshRendL[i].outlines[j].OutlineColor = currTypeCol;
             }
         }
     }
