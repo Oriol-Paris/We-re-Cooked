3de4f2cdec9c08492cd57a6cfa8f10b84a391ab6
B-Dani <daniel.belmonte@enti.cat>
2025-07-12 19:50:05 +0200

    Clients hud

--- Assets/Scripts/Clients/CustomerManager.cs
+++ Assets/Scripts/Clients/CustomerManager.cs
@@ -1,15 +1,28 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+
+[Serializable]
+public class CustomerSpawnPoint
+{
+    public Transform pos;
+    public GameObject customerO;
+}
+
 
 public class CustomerManager : MonoBehaviour
 {
     public static CustomerManager instance;
 
     public GameObject customerPrefab;
-    public Transform spawnPoint;
+    public Transform spawnPoint, freeSpawn;
+    public List<Transform> spawnPoints = new List<Transform>();
+
     public int maxCustomers = 3;
     public float spawnInterval = 10f;
     public float customerWaitTime = 30f;
+
+    public ReceipeContainer recipes;
 
     [SerializeField]
     private List<Customer> customers = new List<Customer>();
@@ -27,40 +40,29 @@ public class CustomerManager : MonoBehaviour
 
         if (spawnTimer >= spawnInterval && customers.Count < maxCustomers)
         {
-            SpawnCustomer();
+            SpawnCustomer(this.spawnPoint);
             spawnTimer = 0;
-        }
-
-        foreach (var customer in customers)
-        {
-            customer.UpdateWait(Time.deltaTime);
-            if (customer.isAngry)
-            {
-                Debug.Log("Angy");
-            }
         }
     }
 
-    void SpawnCustomer()
+    void SpawnCustomer(Transform spawnPoint)
     {
-
         if (customerPrefab != null && spawnPoint != null)
         {
             Instantiate(customerPrefab, spawnPoint.position, Quaternion.identity);
         }
-
-
     }
 
     public void RegisterCustomer(Customer customer)
     {
         if (!customers.Contains(customer))
         {
+            recipes.ResetReceipe();
+            //recipes.RefreshHudList();
+            customer.SetRecipie(recipes.currentR);
             customers.Add(customer);
         }
     }
-
-
 
     public List<Customer> GetAllCustomers()
     {
@@ -78,5 +80,13 @@ public class CustomerManager : MonoBehaviour
     public Customer GetFirstWaitingCustomer()
     {
         return customers.Count > 0 ? customers[0] : null;
+    }
+
+    public void RenewCustomer(Customer c)
+    {
+        freeSpawn = c.GetPos();
+        RemoveCustomer(c);
+        SpawnCustomer(freeSpawn);
+        freeSpawn = null;
     }
 }
