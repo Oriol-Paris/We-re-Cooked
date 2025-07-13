c0a3509a0660945ee8cfdbe19d784b7cbc52a8e5
B-Dani <daniel.belmonte@enti.cat>
2025-07-13 11:18:12 +0200

    Client responses to meals

--- Assets/Scripts/Score/ScoreBar.cs
+++ Assets/Scripts/Score/ScoreBar.cs
@@ -1,9 +1,15 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoreBar : MonoBehaviour
 {
     public Image fillImg;
+
+    public TextMeshProUGUI txt;
+    public string[] clientResponses;
 
     void Start()
     {
@@ -16,6 +22,7 @@ public class ScoreBar : MonoBehaviour
         float ratio = Mathf.Clamp01(currAmount / maxAmount);
         Debug.Log(ratio);
         fillImg.fillAmount = Mathf.Lerp(0.1f, 1, ratio);
+        ClientResponse(ratio);
         CalculateColor();
     }
 
@@ -27,5 +34,20 @@ public class ScoreBar : MonoBehaviour
     public void SetScoreState(bool active)
     {
         transform.GetChild(0).gameObject.SetActive(active);
+    }
+
+    private void ClientResponse(float ratio)
+    {
+        if(ratio < 0.5f)
+        {
+            txt.text = clientResponses[0];
+        }else if(ratio <= 0.75f)
+        {
+            txt.text = clientResponses[0];
+        }
+        else
+        {
+            txt.text = clientResponses[0];
+        }
     }
 }
